4445cb9c9d0e653c5c57fce4046e79ce71a3139a
vishvacrazy <svishva201@gmail.com>
2026-03-11 20:43:56 +0530

    first commit

---
+++ src/Navbr.jsx
@@ -0,0 +1,12 @@
+function Navbar(){
+    return(
+        <nav className="navbar">
+            <h1>My Website</h1>
+            <div className="links">
+                <a href="/">Home</a>
+                <a href="/about">About</a>
+                <a href="/contact">Contact</a>
+            </div>
+        </nav>
+    )
+}
